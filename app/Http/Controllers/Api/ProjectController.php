838c2fd7495a87930ff5181a956ee97c4322ccb4
federicomanni97 <manni_federico@libero.it>
2024-01-19 10:42:55 +0100

    project controller created + show/index

---
+++ app/Http/Controllers/Api/ProjectController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Project;
+
+class ProjectController extends Controller
+{
+    //
+    public function index(){
+        $projects = Project::paginate(3);
+        return response()->json(
+        [
+            'success' => true,
+            'results' => $projects
+        ]
+        );
+    }
+
+    public function show($slug){
+        $project = Project::where('slug', $slug)->with(['category', 'technologies'])->first();
+        return response()->json(
+        [
+            'success' => true,
+            'results' => $project
+        ]
+        );
+    }
+}
